e9fa69be52aa0e79bec734a48c737e7d502b0237
B0BBEY <98026300+B0BBEY@users.noreply.github.com>
2022-10-13 19:37:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,22 +7,26 @@ const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 
-
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=3f76d79e4da3ad151ac0ab63b2b18519'
 const API_UNITS = '&units=metric'
 const API_LANG = '&lang=pl'
+
+const API_MAP_LINK = 'https://api.tomtom.com/map/1/tile/basic/main/10/22/22.png?tileSize=256&view=Unified&language=NGT&key=hctkcGGqPpYlFs33RybdeGCZ8aWwP37Z'
+
 
 const getWeather = () => {
     const city = input.value || 'Warszawa'
     const URL = API_LINK + city + API_KEY + API_LANG + API_UNITS
 
     axios.get(URL).then(res => {
+
         const temp = res.data.main.temp
         const hum = res.data.main.humidity
         const weatherId = res.data.weather[0].id
 
-        console.log(res.data)
+        const lat = res.data.coord.lat
+        const lon = res.data.coord.lon
 
          cityName.textContent = res.data.name 
          temperature.textContent = Math.floor(temp) + '°C'
@@ -49,8 +53,17 @@ const getWeather = () => {
          } else {
             photo.setAttribute('src', 'img/unknown.png')
          }
+
+
+         var map = tt.map({
+            key: 'hctkcGGqPpYlFs33RybdeGCZ8aWwP37Z',
+            container: 'map',
+            center: [lon, lat],
+            zoom: 11
+        });
+
     }).catch(() => warning.textContent = 'Wpisz poprawną nazwę miasta!' )
-    
+     
 }
 
 const enterCheck = (e) => {
@@ -58,7 +71,6 @@ const enterCheck = (e) => {
       getWeather()
    }
 }
-
 
 button.addEventListener('click', getWeather)
 input.addEventListener('keyup', enterCheck)
